d327284f54da64cc8fae293bb3623338e7f517f8
Edgar Gonzalez <edgargonzalez.info@gmail.com>
2021-01-09 20:22:17 +0100

    Some error state cover

--- src/main/scala/orderplacing/Main.scala
+++ src/main/scala/orderplacing/Main.scala
@@ -6,9 +6,15 @@ import scala.io.Source
 object Main {
 
     def main(args: Array[String]): Unit = {
-        val filename = "src/main/scala/orderplacing/place-order.json"
-        val source = Source.fromFile(filename)
-        val responseBody = try source.mkString finally source.close()
-        PlaceOrderApi.placeOrderApi(HttpRequest("GET", "https://myorders.com/placeOrders", responseBody))
+        val fileName = "place-order.json"
+        val path = new java.io.File(s"src/main/scala/orderplacing/$fileName")
+
+        if(path.exists()){
+            val source = Source.fromFile(path)
+            val responseBody = try source.mkString finally source.close()
+            PlaceOrderApi.placeOrderApi(HttpRequest("GET", "<Add Url>", responseBody))
+        }else {
+            println(s" $path does not exists.")
+        }
     }
 }
